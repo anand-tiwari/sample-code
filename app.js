4c0e1cef797eef3c52cbb7635b85d8b66592aa39
anand <anandtiwari192@gmail.com>
2017-01-05 02:55:37 +0530

    changed the logic of image-container

--- app.js
+++ app.js
@@ -59,6 +59,83 @@ var app = angular.module('myApp', []);
 			"top":"1200"
 		}
 	];
+
+
+	$(document).ready(function(){
+		$(".panel").animate({ 
+	        right:'0px'
+	    },1000);
+
+	  	// close icon code 
+	    $(".close").on('click',function(){
+	    	$(this).closest('.panel').animate({
+	    		right:'-354px'
+	    	},1000);
+	    	$(this).siblings('.open').animate({
+	    		height:"18px",
+	    		padding:"2px 13px"
+	    	});
+	    });
+
+	    // open button click event
+	    $(".open").on('click', function(){
+	    	$(this).closest(".panel").animate({ 
+	        	right:'0px'
+	    	},1000);
+	    	$(this).animate({
+	    		height:"0",
+	    		padding:"0"
+	    	});
+	    });
+
+	    $('.img-box').each(function(){
+	    	//set size
+		    var containerHeight = $(this).height(),//box height
+		        containerWidth = $(this).width(),//box width
+		        image = $(this).children('img'),//image
+		        imageHeight = image.height(),//inital image height
+		        imageWidth = image.width();//initial image width
+
+		    if (imageWidth<imageHeight) { //if portrait
+		    	image.addClass('ww').removeClass('wh');//set width 100%
+		    } else { //if lanadscape
+		        image.addClass('wh').removeClass('ww');//set height 100%
+		    }
+
+		    //set offset
+		    var newimageHeight = image.height(),//new image height
+		        newimageWidth = image.width(),//new image width
+		        heightDiff = (newimageHeight-containerHeight)/2,//half dif img/box height
+		        widthDiff = (newimageWidth-containerWidth)/2;//half dif img/box width
+
+		    if (widthDiff>0) {
+		        image.css({marginLeft: '-'+widthDiff+'px', marginTop: 0});//offset left
+		    } else {
+		        image.css({marginTop: '-'+heightDiff+'px', marginLeft: 0});//offset top
+		    }
+
+		});
+	});
 });
 
 
+/*
+
+To resize an image proportionally, you have to set either the height or width to "100%", but not both. 
+If you set both to "100%", your image will be stretched.
+
+case 1: width < height = i.e portrait
+		width : 100%;
+case 2 : width > height: i.e lanadscape
+		height:100%:
+case 3: width == height: 
+		if widht>height  = >  width :100%
+		if height>width  = >  height: 100%;
+	if this case height is greater than width so i didn't add case 3.
+	Included both case 2 & case 3 same.
+
+Then After adding one property(either height or width ) other property is automatically added. 
+Now we have to find the difference of height & width from container. 
+And add margin property accordingly.
+
+*/
