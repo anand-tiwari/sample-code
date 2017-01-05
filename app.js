014985066fe35444d04a923dd2265fae96e2d838
anand <anandtiwari192@gmail.com>
2017-01-05 11:59:47 +0530

    updated

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ var app = angular.module('myApp', []);
 			"img2":"r1.jpg",
 			"type":"Are you looking for 'White dresses' ",
 			"specification":"Cotton White Dresses",
-			"top":"1800"
+			"top":"0"
+		},
+		{
+			"img1":"r2.jpg",
+			"img2":"r3.jpg",
+			"type":"Are you looking for 'White dresses' ",
+			"specification":"Cotton White Dresses",
+			"top":"300"
 		},
 		{
 			"img1":"g2.jpg",
@@ -17,54 +24,74 @@ var app = angular.module('myApp', []);
 			"top":"600"
 		},
 		{
-			"img1":"n2.jpg",
-			"img2":"n3.jpg",
-			"type":"Are you looking for 'White dresses' ",
-			"specification":"Cotton White Dresses",
-			"top":"1500"
-		},
-		{
 			"img1":"b1.jpg",
 			"img2":"b2.jpg",
 			"type":"Are you looking for 'White dresses' ",
 			"specification":"Cotton White Dresses",
-			"top":"0"
+			"top":"900"
 		},
 		{
-			"img1":"r2.jpg",
-			"img2":"r3.jpg",
+			"img1":"n2.jpg",
+			"img2":"n3.jpg",
 			"type":"Are you looking for 'White dresses' ",
 			"specification":"Cotton White Dresses",
-			"top":"2100"
+			"top":"1200"
 		},
 		{
 			"img1":"b3.jpg",
 			"img2":"g1.jpg",
 			"type":"Are you looking for 'White dresses' ",
 			"specification":"Cotton White Dresses",
-			"top":"300"
+			"top":"1500"
 		},
 		{
 			"img1":"h1.jpg",
 			"img2":"h2.jpg",
 			"type":"Are you looking for 'White dresses' ",
 			"specification":"Cotton White Dresses",
-			"top":"900"
+			"top":"1800"
 		},
 		{
 			"img1":"h3.jpg",
 			"img2":"n1.jpg",
 			"type":"Are you looking for 'White dresses' ",
 			"specification":"Cotton White Dresses",
-			"top":"1200"
+			"top":"2100"
 		}
 	];
-
 
 	$(document).ready(function(){
 		$(".panel").animate({ 
 	        right:'0px'
 	    },2000);
+
+	var imagePosition = function(){
+		$('.img-box').each(function(){
+		    	//set size
+		    var containerHeight = $(this).height(),//box height
+		        containerWidth = $(this).width(),//box width
+		        image = $(this).children('img'),//image
+		        imageHeight = image.height(),//inital image height
+		        imageWidth = image.width();//initial image width
+
+		    if (imageWidth<imageHeight) { //if portrait
+		    	image.addClass('ww').removeClass('wh');//set width 100%
+		    } else { //if lanadscape
+		        image.addClass('wh').removeClass('ww');//set height 100%
+		    }
+		    //set offset
+		    var newimageHeight = image.height(),//new image height
+		        newimageWidth = image.width(),//new image width
+		        heightDiff = (newimageHeight-containerHeight)/2,//half dif img/box height
+		        widthDiff = (newimageWidth-containerWidth)/2;//half dif img/box width
+
+		    if (widthDiff>0) {
+		        image.css({marginLeft: '-'+widthDiff+'px', marginTop: 0});//offset left
+		    } else {
+		        image.css({marginTop: '-'+heightDiff+'px', marginLeft: 0});//offset top
+		    }
+		});
+	};
 
 	  	// close icon code 
 	    $(".close").on('click',function(){
@@ -87,36 +114,9 @@ var app = angular.module('myApp', []);
 	    		padding:"0"
 	    	});
 	    });
-
-	    setTimeout(function(){
-		    $('.img-box').each(function(){
-		    	//set size
-			    var containerHeight = $(this).height(),//box height
-			        containerWidth = $(this).width(),//box width
-			        image = $(this).children('img'),//image
-			        imageHeight = image.height(),//inital image height
-			        imageWidth = image.width();//initial image width
-
-			    if (imageWidth<imageHeight) { //if portrait
-			    	image.addClass('ww').removeClass('wh');//set width 100%
-			    } else { //if lanadscape
-			        image.addClass('wh').removeClass('ww');//set height 100%
-			    }
-			    //set offset
-			    var newimageHeight = image.height(),//new image height
-			        newimageWidth = image.width(),//new image width
-			        heightDiff = (newimageHeight-containerHeight)/2,//half dif img/box height
-			        widthDiff = (newimageWidth-containerWidth)/2;//half dif img/box width
-
-			    if (widthDiff>0) {
-			        image.css({marginLeft: '-'+widthDiff+'px', marginTop: 0});//offset left
-			    } else {
-			        image.css({marginTop: '-'+heightDiff+'px', marginLeft: 0});//offset top
-			    }
-			});
-		},500);
-
+	    imagePosition();
 	});
+	
 });
 
 
